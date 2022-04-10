aa23c8b8b001a5d49991229a266eb32b7616552c
hsy <975892929@qq.com>
2022-04-11 02:39:02 +0300

    A clean and pure beginning :-)

---
+++ src/main/java/com/doit/forfutherproject/ForFutureProjectApplication.java
@@ -0,0 +1,13 @@
+package com.doit.forfutherproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForFutureProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForFutureProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doit/forfutherproject/ForFutureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doit.forfutherproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForFutureProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
